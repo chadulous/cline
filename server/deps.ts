01df0deb2c2bbdcd310ad4b46386bdfdc14627ab
mavdotjs <consorceo300@icloud.com>
2023-11-12 01:27:47 -0500

    Initial commit

---
+++ server/deps.ts
@@ -0,0 +1,3 @@
+export * as auth from "npm:lucia@2.7.4";
+import express from "https://esm.sh/express@4.18.2";
+export { express };
---
+++ server/main.ts
@@ -0,0 +1,5 @@
+import { auth, express } from "./deps.ts";
+
+const app = express();
+
+app.listen(80, '0.0.0.0')
